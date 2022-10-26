6276e3096b73ca3314c996f24b35e633de9051f9
Elladio <elladio.m21@gmail.com>
2022-10-26 19:14:25 -0400

    RENDER ABOUT TO WORK

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 
-const basedUrl = "https://api.disneyapi.dev/"
-// const disneyUrl = $.ajax("https://api.disneyapi.dev/characters").then((data) => {
-//     return data
-// });
+const disneyUrl = "https://api.disneyapi.dev/character?name="
+const $main = $("main");
+$main.append($("<div>").html(`<p>Who? :</p>`));
+$main.append($("<div>").html(`<p>Where? :</p>`));
+$main.append($("<div>").html(`<p>Related materials?</p>`));
 
-function characterSearch(){
-    const search = document.getElementById("search").value;
-    const url = '${basedUrl}?q=${search}`
-    $.ajax({
-        url: url,
-        type: "GET",
-        dataType: "json",
-})}
-console.log(characterSearch)
+
+
+// function charSearch(character) {
+//     const url = `${disneyUrl}${character}`;
+//     $.ajax(url)
+//     .then((data) => {
+//         $main.empty();
+//         $main.append($("<div>").html(`<p> Who?: ${data[8].name}</p>`))
+//     })
+// }
+// function characterSearch(character){
+//     const url = `${disneyUrl}${character}}`
+//     $.ajax(url).then((data) => {
+//         console.log(data)
+//         disData = data
+//             $main.empty();
+//             // $main.append($("<div>").html(`<p> Who?: ${disData.data.name}</p>`))
+// })
+// }
+
+function characSearch(char){
+    const url = `${disneyUrl}${char}`;
+    $.ajax(url).then((data) => {
+        console.log(data)
+        $main.empty();
+        //$main.append($("<div>").html(`<p> Who?: ${data[8].name}</p>`))
+})
+}
+       
+$("input[type=submit]").on("click", (event) =>{
+    event.preventDefault()
+    const innerText = $("input[type=text]").val()
+    characSearch(innerText);
+});
