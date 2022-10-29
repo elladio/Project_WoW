06c765f6cbd5121f3ce9918c69511518f6ece629
Elladio <elladio.m21@gmail.com>
2022-10-28 22:39:32 -0400

    images load site works formating is odd need list

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const disneyUrl = "https://api.disneyapi.dev/character?name="
+
 // const $main = $("main");
 // $main.append($("<div>").html(`<p>Who? :</p>`));
 // $main.append($("<div>").html(`<p>Where? :</p>`));
@@ -10,16 +11,19 @@ function characSearch(char){
     const url = `${disneyUrl}${char}`;
     $.ajax(url).then((disneyData) => {
         console.log(disneyData)
-        const $main = $("div.response");
+        const $main = $(".child3");
         $main.empty();
-        $main.append($("<div>").html(`<p> Who?: ${disneyData.data[0].name}</p>`))
-        $main.append($("<div>").html(`<p>Where? ${disneyData.data[0].films}:</p>`));
+        $main.append($("<div>").html(`<p> Who?: ${disneyData.data[0].name} duhh!</p>`))
+        $main.append($("<div>").html(`<p>They're in...${disneyData.data[0].films}</p>`));
         $main.append($("<div>").html(`<p> Also in!: ${disneyData.data[0].shortFilms}</p>`))
-        $main.append($("<div>").html(`
-        <p>Related materials?</p>">`))
-})
+        $main.append($("<div>").html(`<h1> They dont like: </h1>
+        <p>${disneyData.data[0].enemies}</p>`))
+        $main.append($("<div>").html(`<img src="${disneyData.data[0].imageUrl}"></img><p> Looks something like this ^</p>`)
+    
+    )})
 }
  // how to make case insensitive
+
 
 $("input[type=submit]").on("click", (event) =>{
     event.preventDefault()
