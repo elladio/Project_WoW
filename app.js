f9a2d3f5090dd532517fe862051b9eb442439ad0
Elladio <elladio.m21@gmail.com>
2022-10-29 10:09:34 -0400

    cleaned up

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ function characSearch(char){
         console.log(disneyData)
         const $main = $(".child3");
         $main.empty();
-        $main.append($("<div>").html(`<p> Who?: ${disneyData.data[0].name} duhh!</p>`))
-        $main.append($("<div>").html(`<p>They're in...${disneyData.data[0].films}</p>`));
-        $main.append($("<div>").html(`<p> Also in!: ${disneyData.data[0].shortFilms}</p>`))
-        $main.append($("<div>").html(`<h1> They dont like: </h1>
-        <p>${disneyData.data[0].enemies}</p>`))
-        $main.append($("<div>").html(`<img src="${disneyData.data[0].imageUrl}"></img><p> Looks something like this ^</p>`)
+        $main.append($("<div>").html(`<h2> Who?: </h2><p>${disneyData.data[0].name} duhh!</p>`))
+        $main.append($("<div>").html(`<h2>They're in...</h2> <p>${disneyData.data[0].films}</p>`));
+        $main.append($("<div>").html(`<h2> Also in!:</h2> <p>${disneyData.data[0].shortFilms}</p>`))
+        $main.append($("<div>").html(`<h2> On the small screen?:</h2> <p>${disneyData.data[0].tvShows}</p>`))
+        $main.append($("<div>").html(`<h2> They're buddies: </h2>
+        <p>${disneyData.data[0].allies}</p>`))
+        $main.append($("<div>").html(`<h2> Looks something like this </h2> <img src="${disneyData.data[0].imageUrl}"></img>`)
     
     )})
 }
